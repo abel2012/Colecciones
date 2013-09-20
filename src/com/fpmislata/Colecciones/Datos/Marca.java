6d37107c6a6e63d53c9fd13d698f5db7778a020b
abel2012 <abelpfon2010@gmail.com>
2013-09-20 10:28:50 +0200

    hola

---
+++ src/com/fpmislata/Colecciones/Datos/Marca.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fpmislata.Colecciones.Datos;
+
+/**
+ *
+ * @author alumno
+ */
+public enum Marca {
+    Seat, 
+    Renault,
+    Ford,
+    Citroen
+}
