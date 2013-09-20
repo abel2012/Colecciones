94f4e671b95d8a1302da43e3d30347e0ff63eb17
abel2012 <abelpfon2010@gmail.com>
2013-09-20 11:02:21 +0200

    hola

--- src/colecciones/Colecciones.java
+++ src/colecciones/Colecciones.java
@@ -20,20 +20,25 @@ public class Colecciones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        Map<String,Coche> mapCoches=new HashMap<>();
-        Coche coche1= new Coche(Marca.FORD,"MONDEO",200,"53752456Y");
-        Coche coche2= new Coche(Marca.FORD,"Fiesta",237,"45678923I");
-        Coche coche3= new Coche(Marca.CITROEN,"Xantia",324,"23456712D");
-        Coche coche4= new Coche(Marca.RENAULT,"21",123,"21456723D");
-        Coche coche5= new Coche(Marca.SEAT,"Ibiza",160,"45678923T");
-        
-        mapCoches.put(coche1.getDniPropietario(),coche1);
-        
-        Coche cocheBuscado=mapCoches.get("Fiesta");
+
+        Map<String, Coche> mapCoches = new HashMap<>();
+        Coche coche1 = new Coche(Marca.FORD, "MONDEO", 200, "53752456Y");
+        Coche coche2 = new Coche(Marca.FORD, "Fiesta", 237, "45678923I");
+        Coche coche3 = new Coche(Marca.CITROEN, "Xantia", 324, "23456712D");
+        Coche coche4 = new Coche(Marca.RENAULT, "21", 123, "21456723D");
+        Coche coche5 = new Coche(Marca.SEAT, "Ibiza", 160, "45678923T");
+
+        mapCoches.put(coche1.getDniPropietario(), coche1);
+        mapCoches.put(coche2.getDniPropietario(), coche2);
+        mapCoches.put(coche3.getDniPropietario(), coche3);
+        mapCoches.put(coche4.getDniPropietario(), coche4);
+        mapCoches.put(coche5.getDniPropietario(), coche5);
+
+
+
+        Coche cocheBuscado = mapCoches.get("45678923I");
         System.out.println(cocheBuscado.getMarca());
-        
-        
+
+
     }
-    
 }
